bf4121ee7316052afab557406d1e56781ae2c3d7
joshuaaduarte <jad6442@tamu.edu>
2022-06-07 21:41:59 -0500

    Added script that allows user to communicate with arduino over python.

---
+++ Arduino/pythonCom/main.py
@@ -0,0 +1,30 @@
+import serial
+import time
+
+arduinoData = serial.Serial('COM4',9600)
+arduinoData.timeout = 1
+
+# def selectDirection():
+#     arduinoData.write(str(input("What direction?")))
+#
+# def selectRotations():
+#     arduinoData.write(int(input("How many rotations?")))
+#
+#
+# selectDirection()
+# selectRotations()
+
+while True:
+    i = input("What Direction: ").strip()
+    if i == 'done':
+        print("Program done")
+        break
+    arduinoData.write(i.encode())
+    time.sleep(.5)
+
+    j = input("How many Rotations: ").strip()
+    arduinoData.write(j.encode())
+    time.sleep(1)
+
+
+arduinoData.close()
