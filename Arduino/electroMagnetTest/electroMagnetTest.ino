db458ed3c1f908bc8946e29a5a2fe4273b27b9b2
joshuaaduarte <jad6442@tamu.edu>
2022-07-05 07:36:59 -0500

    Updated model to be more accurate and functioning.

---
+++ Arduino/electroMagnetTest/electroMagnetTest.ino
@@ -0,0 +1,13 @@
+const int pin = 3;
+//Recuerda que debes usar el pin correcto que hayas utilizado en el esquema eléctrico de tu proyecto
+  
+void setup() {
+  pinMode(pin, OUTPUT);  //definir pin como salida
+}
+  
+void loop(){
+  digitalWrite(pin, HIGH);   // poner el Pin en HIGH para activar el electroimán
+  delay(10000);               // esperar un segundo
+  digitalWrite(pin, LOW);    // poner el Pin en LOW para desactivar el electroimán
+  delay(10000);               // esperar un segundo
+}
